6c52e39a33ff762ea10dced9f8a7ced4d572e490
Rachel Bryk <RachelBryk@gmail.com>
2014-07-11 19:55:01 -0400

    If no file is passed, use nwc24msg.cfg in current directory if it exists. if not, check in global user directory.

--- UnbanMii/UnbanMii/UnbanMii.cpp
+++ UnbanMii/UnbanMii/UnbanMii.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <time.h>
 #include "UnbanMii.h"
+#include <ShlObj.h>
 
 
 u32 CalculateNwc24ConfigChecksum(nwc24_config_t config)
@@ -21,13 +22,43 @@ u32 CalculateNwc24ConfigChecksum(nwc24_config_t config)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	std::wstring filename;
 	if (argc != 2)
 	{
-		std::cout << "no file specified";
-		return 0;
-	}
+		std::ifstream file(L"nwc24msg.cfg");
 
-	std::wstring filename = argv[1];
+		std::ifstream global_file;
+		bool global_exists = false;
+
+		WCHAR my_documents[MAX_PATH];
+		bool my_documents_found = SUCCEEDED(SHGetFolderPath(nullptr, CSIDL_MYDOCUMENTS, nullptr, SHGFP_TYPE_CURRENT, my_documents));
+		if (my_documents_found)
+		{
+			global_file.open(std::wstring(my_documents) + L"/Dolphin Emulator/Wii/shared2/wc24/nwc24msg.cfg");
+			if (global_file.is_open())
+				global_exists = true;
+		}
+
+
+		if (file.is_open())
+		{
+			std::cout << "Using nwc24msg.cfg in current directory.\n";
+			filename = L"nwc24msg.cfg";
+		}
+		else if (global_exists)
+		{
+			std::cout << "Using nwc24msg.cfg in global user directory.\n";
+			filename = std::wstring(my_documents) + L"/Dolphin Emulator/Wii/shared2/wc24/nwc24msg.cfg";
+		}
+		else
+		{
+			std::cout << "no file specified";
+			return 0;
+		}
+	}
+	else
+		filename = argv[1];
 
 	if (filename.find(L"nwc24msg.cfg") == std::string::npos)
 	{
